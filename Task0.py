39d13a91cfa729308b47bdaa52a5f598648e1940
R Alda-Fernandes <LifeBringer@users.noreply.github.com>
2019-05-11 19:53:16 -0400

    Update Task0.py

--- Task0.py
+++ Task0.py
@@ -12,14 +12,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-
-"""
-TASK 0:
-What is the first record of texts and what is the last record of calls?
-Print messages:
-"First record of texts, <incoming number> texts <answering number> at time <time>"
-"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
-"""
 def get_problemzero(calls, texts):
     # Records to locate
     first_text, last_call = 0, -1
