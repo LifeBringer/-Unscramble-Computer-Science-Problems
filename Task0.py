9a7e9b1b3c0f8938c8cc9c2ef572039e78d54ec7
R Alda-Fernandes <LifeBringer@users.noreply.github.com>
2019-05-16 01:26:59 -0400

    Updated with PEP8 styling

--- Task0.py
+++ Task0.py
@@ -1,9 +1,11 @@
+
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -12,13 +14,17 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+
 def get_problemzero(calls, texts):
     # Records to locate
     first_text, last_call = 0, -1
- 
+
     # Print Solution
-    print("First record of texts, {0} texts {1} at time {2}".format(*texts[first_text]))
-    print("Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds".format(*calls[last_call]))
+    print("First record of texts, {0} texts {1} at time {2}".format(
+        *texts[first_text]))
+    print("Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds".format(
+        *calls[last_call]))
     return 0
+
 
 get_problemzero(calls, texts)
