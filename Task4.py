40fc597fd7c6a0a738829385d0b63f280f3e6a87
R Alda-Fernandes <LifeBringer@users.noreply.github.com>
2019-05-16 01:38:26 -0400

    PEP8 and time complexity improvement
    Updated to adhere to PEP8 and changed lists into sets to quickly generate a set of unique numbers.

--- Task4.py
+++ Task4.py
@@ -22,14 +22,17 @@ receive texts or receive incoming calls.
 Print a message:
 "These numbers could be telemarketers: "
 <list of numbers>
-The list of numbers should be print out one per line in lexicographic order with no duplicates.
+The list of numbers should be print out one per line in lexicographic order
+with no duplicates.
 """
 
+
 def get_problemfour(call_log, text_log):
-    '''Takes call log and text log and provides a list of possible telemarketers'''
+    '''Takes call log and text log and provides a list of possible
+    telemarketers'''
     # List of area codes in Bangalore
     suspect_list = []
-    texter_list = get_list_of_texters(text_log)
+    texter_textee_list = get_list_of_texters_textees(text_log)
     answering_list = get_list_of_answering_nums(call_log)
 
     # Columns extracted
@@ -41,20 +44,22 @@ def get_problemfour(call_log, text_log):
     # Check Call log
     for entry in calls:
         number_on_trial = entry[incoming_number]
-        if number_on_trial not in texter_list and number_on_trial not in answering_list:
+        if number_on_trial not in texter_textee_list and number_on_trial not in \
+                answering_list:
             suspect_list.append(number_on_trial)
-    
+
     # Output
     print("These numbers could be telemarketers: ")
     suspect_list.sort()
     for suspect in suspect_list:
-      print(suspect)
+        print(suspect)
 
     return -1
 
-def get_list_of_texters(text_log):
+
+def get_list_of_texters_textees(text_log):
     # Catalog of unique numbers
-    catalog = []
+    catalog = set()
 
     # Columns extracted
     incoming_number, answering_number = 0, 1
@@ -64,17 +69,15 @@ def get_list_of_texters(text_log):
 
     # Check Text log
     for entry in texts:
-        if entry[incoming_number] not in catalog:
-            catalog.append(entry[incoming_number])
-        if entry[answering_number] not in catalog:
-            catalog.append(entry[answering_number])
+        catalog.add(entry[incoming_number])
+        catalog.add(entry[answering_number])
 
     return catalog
 
 
 def get_list_of_answering_nums(call_log):
     # Catalog of unique numbers
-    answer_catalog = []
+    answer_catalog = set()
 
     # Columns extracted
     answering_number = 1
@@ -84,10 +87,8 @@ def get_list_of_answering_nums(call_log):
 
     # Check Text log
     for entry in calls:
-        if entry[answering_number] not in answer_catalog:
-            answer_catalog.append(entry[answering_number])
+        answer_catalog.add(entry[answering_number])
 
     return answer_catalog
 
-# Process Problem 4
 get_problemfour(calls, texts)
