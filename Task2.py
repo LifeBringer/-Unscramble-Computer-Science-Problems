b474d445b1061bfdc56637431a159c13b3cf53d8
R Alda-Fernandes <LifeBringer@users.noreply.github.com>
2019-05-16 01:28:33 -0400

    PEP8 and corrected error
    Updated to correct interpretation of the problem, follow PEP8 styling, and remove error caused by using Python 2.7 vs 3.7 to compile.

--- Task2.py
+++ Task2.py
@@ -16,33 +16,37 @@ TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
 Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
-September 2016.".
+"<telephone number> spent the longest time, <total time> seconds, on the phone
+during September 2016.".
 """
 
-def get_problemtwo(call_log):
-    '''Takes in caller log from 2016 and outputs the longest conversation duration initiating caller'''
-    # Biggest talker aka longest call
-    biggest_talker = []
 
-    # Record the longest duration
-    longest_duration = 0
+def get_problemtwo(call_log):
+    '''Takes in caller log from 2016 and outputs the longest conversation \
+        duration initiating caller'''
+    # Biggest talker aka longest call
+    biggest_talker = {}
 
     # Columns extracted
-    incoming_number, duration = 0, 3
+    incoming_number, answering_number, duration = 0, 1, 3
 
     # Assign Logs
     calls = call_log
 
     # Check Call log
     for entry in calls:
-        longer_call = entry[duration] > longest_duration
-        if longer_call:
-            biggest_talker = entry[incoming_number]
-            longest_duration = entry[duration]
+            if entry[answering_number] in biggest_talker:
+                biggest_talker[entry[answering_number]] += int(entry[duration])
+            else:
+                biggest_talker[entry[answering_number]] = int(entry[duration])
+            if entry[incoming_number] in biggest_talker:
+                biggest_talker[entry[incoming_number]] += int(entry[duration])
+            else:
+                biggest_talker[entry[incoming_number]] = int(entry[duration])
 
     # Print Solution
-    print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(biggest_talker,longest_duration))
+    print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(
+        max(biggest_talker, key=biggest_talker.get), max(biggest_talker.values())))
 
     return 0
 
